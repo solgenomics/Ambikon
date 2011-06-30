0c424f65e17e7d84a706097e2fb0130234c306b8
Robert Buels <rmb32@cornell.edu>
2011-06-30 13:58:47 -0700

    add Ambikon::Xref object

---
+++ lib/Ambikon/Xref.pm
@@ -0,0 +1,130 @@
+package Ambikon::Xref;
+# ABSTRACT: object representing a single Ambikon xref
+use Moose;
+use namespace::autoclean;
+
+use MooseX::Aliases;
+use MooseX::Types::URI 'Uri';
+
+=attr url
+
+the absolute or relative URL where the full resource can be accessed,
+e.g. /unigenes/search.pl?q=SGN-U12
+
+=cut
+
+has 'url' => (
+    is  => 'ro',
+    isa => Uri,
+    required => 1,
+    coerce => 1,
+   );
+
+
+=attr text
+
+a short text description of the contents of the resource referenced,
+for example "6 SGN Unigenes"
+
+=cut
+
+has 'text' => (
+    is  => 'ro',
+    isa => 'Str',
+    required => 1,
+   );
+
+
+=attr is_empty
+
+true if the cross reference is empty, may be used as a rendering hint
+
+=cut
+
+has 'is_empty' => (
+    is  => 'ro',
+    isa => 'Bool',
+    default => 0,
+   );
+
+=attr subsite
+
+the subsite object this cross reference points to
+
+=cut
+
+has 'subsite' => (
+    is => 'ro',
+    required => 1,
+    alias => 'feature',
+   );
+
+=attr renderings
+
+2-level hashref of suggested renderings, keyed first by content type,
+then rendering hint
+
+=cut
+
+has 'renderings' => (
+   is      => 'ro',
+   isa     => 'HashRef',
+   default => sub { +{} },
+ );
+
+sub TO_JSON {
+    my ( $self ) = @_;
+    no strict 'refs';
+    return {
+        feature => $self->feature->feature_name,
+        map { $_ => $self->$_() }
+        qw( url is_empty text renderings )
+    };
+}
+
+sub xref_cmp {
+    my ( $a, $b ) = @_;
+    no warnings 'uninitialized';
+    return
+        $a->feature->feature_name cmp $b->feature->feature_name
+     || $a->is_empty <=> $b->is_empty
+     || $a->text cmp $b->text
+     || $a->url.'' cmp $b->url.'';
+}
+# alias xref_cmp to cr_cmp for backcompat
+alias 'cr_cmp' => 'xref_cmp';
+
+sub xref_eq {
+    my ( $a, $b ) = @_;
+
+    no warnings 'uninitialized';
+    return !($a->is_empty xor $b->is_empty)
+        && $a->text eq $b->text
+        && $a->url.'' eq $b->url.''
+        && $a->feature->feature_name eq $b->feature->feature_name;
+}
+# alias xref_eq to cr_eq for backcompat
+alias 'cr_eq' => 'xref_eq';
+
+sub uniq {
+    my %seen;
+    grep !$seen{ _uniq_str($_) }++, @_;
+}
+sub _uniq_str {
+    my ( $self ) = @_;
+    return join ',', (
+        $self->feature->feature_name,
+        $self->url,
+        $self->text,
+       );
+}
+
+{ no warnings 'once';
+  *distinct = \&uniq;
+}
+
+
+1;
+
+
+
---
+++ t/xref.t
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Test::MockObject;
+
+use_ok( 'Ambikon::Xref' );
+
+my $mock_feature = Test::MockObject->new;
+$mock_feature->set_always( 'feature_name', 'fakefeature' );
+
+my $cr1 = Ambikon::Xref->new({
+    url  => '/foo/bar.txt',
+    text => 'Noggin',
+    feature => $mock_feature,
+});
+
+my $cr2 = Ambikon::Xref->new({
+    url  => '/foo/bar.txt',
+    text => 'Noggin',
+    feature => $mock_feature,
+});
+
+my $cr3 = Ambikon::Xref->new({
+    url  => '/foo/baz.txt',
+    text => 'Noggin',
+    feature => $mock_feature,
+});
+
+ok(   $cr2->cr_eq( $cr1 ), 'cr eq finds equal'     );
+ok( ! $cr2->cr_eq( $cr3 ), 'cr eq finds not equal' );
+
+is( $cr2->cr_cmp( $cr1 ),  0, 'cr cmp for eq' );
+is( $cr2->cr_cmp( $cr3 ), -1, 'cr cmp 1' );
+is( $cr3->cr_cmp( $cr1 ),  1, 'cr cmp 2' );
+
+my @u = $cr1->uniq( $cr2, $cr3 );
+is( scalar(@u), 2, 'uniq seems to work 0' );
+is( $cr1,   $u[0], 'uniq seems to work 1' );
+is( $cr3,   $u[1], 'uniq seems to work 2' );
+
+my $js_hash = $cr3->TO_JSON;
+is( $js_hash->{text}, 'Noggin' );
+can_ok( $js_hash->{url}, 'scheme', 'path' );
+is( $js_hash->{feature}, 'fakefeature' );
+
+
+
+done_testing;
