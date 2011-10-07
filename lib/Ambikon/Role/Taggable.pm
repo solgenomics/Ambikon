ca35a348714a6237222093c89c42e088e7c8ddd4
Robert Buels <rmb32@cornell.edu>
2011-10-07 16:35:49 -0700

    add find_tag and has_tag methods to taggable role

--- lib/Ambikon/Role/Taggable.pm
+++ lib/Ambikon/Role/Taggable.pm
@@ -23,6 +23,15 @@ Get the tags as a list.  Equivalent to C<< @{ $obj->tags } >>.
 Get the first (primary) tag in the tag list.  Equivalent to
 C<< $obj->tags->[0] >>.
 
+=method find_tag('my_tag')
+
+Find and return the first matching tag in this object's set of tags.
+Matches are case insensitive.  Returns nothing if not found.
+
+=method has_tag('my_tag')
+
+Same as find_tag, but returns 1 or 0 instead of the actual tag.
+
 =cut
 
 has 'tags' => (
@@ -34,9 +43,19 @@ has 'tags' => (
    handles => {
        add_tag     => 'push',
        tag_list    => 'elements',
+       first_tag    => 'first',
    },
 );
 
 sub primary_tag { shift->tags->[0] }
+
+sub find_tag {
+    my ( $self, $tag ) = @_;
+    return $self->first_tag( sub { lc $_ eq lc $tag } );
+}
+
+sub has_tag {
+    shift->find_tag( @_ ) ? 1 : 0
+}
 
 1;
--- t/subsite.t
+++ t/subsite.t
@@ -16,6 +16,8 @@ my $ss = Ambikon::Subsite->new(
 can_ok $ss, qw/shortname name internal_url external_path tags/;
 
 is( $ss->tags->[0], 'fogger', 'tags coercion works' );
+ok( $ss->has_tag('fogger'), 'has_tag seems to work');
+ok( !$ss->has_tag('ziggy'), 'has_tag seems to work 2');
 is( $ss->description, '', 'has an empty description' );
 is( ref( $ss->TO_JSON ), 'HASH', 'TO_JSON makes a hashref' );
 
