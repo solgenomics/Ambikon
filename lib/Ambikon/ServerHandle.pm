017177253fb81c6852d85f0af7e129ff501b6212
Robert Buels <rmb32@cornell.edu>
2011-10-10 10:59:47 -0700

    factor object inflation out of serverhandle into a separate class

--- lib/Ambikon/ServerHandle.pm
+++ lib/Ambikon/ServerHandle.pm
@@ -5,12 +5,10 @@ use Moose;
 use namespace::autoclean;
 use MooseX::Types::URI 'Uri';
 
-use Data::Visitor::Callback;
-use List::MoreUtils 'uniq';
-
 use Ambikon::Subsite;
 use Ambikon::Xref;
 use Ambikon::XrefSet;
+use Ambikon::Xref::Inflator;
 
 =attr base_url
 
@@ -124,81 +122,11 @@ sub search_xrefs {
     return $data;
 }
 
-=method inflate( \%data )
-
-Modifies the given hashref of Ambikon xref result data in-place,
-inflating L<Ambikon::XrefSet>, L<Ambikon::Xref>, and
-L<Ambikon::Subsite> objects from their data-structure representations
-if necessary.
-
-Ignores objects that have already been inflated.
-
-=cut
-
-*inflate_xref_search_result = \&inflate;
-
-sub inflate {
-    my ( $self, $data, $extra_data ) = @_;
-    # shallow-clone extra_data so we can modify it
-    $extra_data = { %{ $extra_data || {} } };
-
-    Data::Visitor::Callback
-        ->new(
-            ignore_return_values => 1,
-            array => sub {
-                my ( $v, $ar ) = @_;
-                for ( @$ar ) {
-                    $v->visit( $_ );
-                    if ( my $i = $self->_inflate( $extra_data, $_ ) ) {
-                        $_ = $i;
-                    }
-                }
-            },
-            hash  => sub {
-                my ( $v, $hr ) = @_;
-                # NOTE: code below is complicated a bit by the need to
-                # make sure we inflate the subsite first, if present,
-                # so we can pass it to any other inflated objects as
-                # an attribute
-                for my $k ( uniq 'subsite', keys %$hr) {
-                    next unless $hr->{$k};
-
-                    $v->visit( $hr->{$k} );
-                    if ( my $i = $self->_inflate( $extra_data, $hr->{$k} ) ) {
-                        $hr->{$k} = $i;
-                    }
-                    if( $k eq 'subsite' ) {
-                        $extra_data->{subsite} = $hr->{$k};
-                    }
-                }
-            },
-          )
-        ->visit( $data );
-
-    return $data;
-}
-
-sub _inflate {
-    my ( $self, $extra, $obj ) = @_;
-
-    ref $obj eq 'HASH' and ( my $classes = $obj->{__CLASS__} )
-        or return;
-
-    $classes = [$classes] unless ref $classes;
-
-    my $error;
-    for my $class ( @$classes ) {
-        # load the most specific class that we can
-        eval { Class::MOP::load_class( $class ) };
-        if( $@ ) { $error = $@; next }
-        my $thawed = eval { $class->thaw( { %$extra, %$obj } ) };
-        if( $@ ) { $error = $@; next }
-        return $thawed;
-    }
-
-    warn "warning, could not inflate object: $error";
-    return $obj;
-}
+has 'xref_inflator' => (
+    is => 'ro',
+    default => sub { Ambikon::Xref::Inflator->new },
+    handles => [ 'inflate' ],
+);
 
 ######## helper methods #########
 
---
+++ lib/Ambikon/Xref/Inflator.pm
@@ -0,0 +1,83 @@
+package Ambikon::Xref::Inflator;
+use Moose;
+
+use List::MoreUtils 'uniq';
+
+use Data::Visitor::Callback;
+
+=method inflate( \%data )
+
+Modifies the given hashref of Ambikon xref result data in-place,
+inflating L<Ambikon::XrefSet>, L<Ambikon::Xref>, and
+L<Ambikon::Subsite> objects from their data-structure representations
+if necessary.
+
+Ignores objects that have already been inflated.
+
+=cut
+
+sub inflate {
+    my ( $self, $data, $extra_data ) = @_;
+    # shallow-clone extra_data so we can modify it
+    $extra_data = { %{ $extra_data || {} } };
+
+    Data::Visitor::Callback
+        ->new(
+            ignore_return_values => 1,
+            array => sub {
+                my ( $v, $ar ) = @_;
+                for ( @$ar ) {
+                    $v->visit( $_ );
+                    if ( my $i = $self->_inflate( $extra_data, $_ ) ) {
+                        $_ = $i;
+                    }
+                }
+            },
+            hash  => sub {
+                my ( $v, $hr ) = @_;
+                # NOTE: code below is complicated a bit by the need to
+                # make sure we inflate the subsite first, if present,
+                # so we can pass it to any other inflated objects as
+                # an attribute
+                for my $k ( uniq 'subsite', keys %$hr) {
+                    next unless $hr->{$k};
+
+                    $v->visit( $hr->{$k} );
+                    if ( my $i = $self->_inflate( $extra_data, $hr->{$k} ) ) {
+                        $hr->{$k} = $i;
+                    }
+                    if( $k eq 'subsite' ) {
+                        $extra_data->{subsite} = $hr->{$k};
+                    }
+                }
+            },
+          )
+        ->visit( $data );
+
+    return $data;
+}
+
+sub _inflate {
+    my ( $self, $extra, $obj ) = @_;
+
+    ref $obj eq 'HASH' and ( my $classes = $obj->{__CLASS__} )
+        or return;
+
+    $classes = [$classes] unless ref $classes;
+
+    my $error;
+    for my $class ( @$classes ) {
+        # load the most specific class that we can
+        eval { Class::MOP::load_class( $class ) };
+        if( $@ ) { $error = $@; next }
+        my $thawed = eval { $class->thaw( { %$extra, %$obj } ) };
+        if( $@ ) { $error = $@; next }
+        return $thawed;
+    }
+
+    warn "warning, could not inflate object: $error";
+    return $obj;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
